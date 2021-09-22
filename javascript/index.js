46b05ab4f593af1affbb57c2b10351e6a783eb7c
JesusHernandez <jesushernandez120491@gmail.com>
2021-09-22 19:37:11 -0300

    avance 2

--- javascript/index.js
+++ javascript/index.js
@@ -4,3 +4,16 @@ $(window).on("hashchange", function () {
   router();
 });
 /*----------------------------------------------------------------------------------------*/
+/*--------------------------------- Navbar -----------------------------------------------*/
+$(function(){
+	var navbar = $('.scrolling-navbar');
+	
+	$(window).scroll(function(){
+		if($(window).scrollTop() <= 40){
+			navbar.removeClass('navbar-scroll');
+		} else {
+			navbar.addClass('navbar-scroll');
+		}
+	});
+});
+/*----------------------------------------------------------------------------------------*/
